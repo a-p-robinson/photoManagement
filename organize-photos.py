6c16706704c243e026ce885b6d849416eda9479e
a-p-robinson <a.p.robinson@gmail.com>
2015-05-31 15:43:43 +0100

    Fixed the problem with not preserving the file extension.
    Added an option to change the destination directory.
    
    Added a Date and Time output for logging.

--- organize-photos.py
+++ organize-photos.py
@@ -6,12 +6,11 @@ import os.path
 import argparse
 from datetime import datetime
 from PIL import Image
+import time
 
 ######################## Variables #########################
 # Where to put the new files
-#destDir = os.environ['HOME'] + '/Dropbox/Photos'
-#destDir = '/data/Photos'
-destDir = '/data/test'
+destDir = '/data/Photos'
 errorDir = destDir + '/Unsorted/'
 
 # The format for the new file names.
@@ -42,15 +41,22 @@ def photoDate(f):
 
 ###################### Main program ########################
 
+print (time.strftime("%d/%m/%Y %H:%M:%S"))
+
 # Define the arguments we want (and optional arguments)
 parser = argparse.ArgumentParser()
 parser.add_argument("pathString", help="Path to Process")
 parser.add_argument("-m", "--move", help="Move the files (default = copy)", action="store_true")
+parser.add_argument("-d", "--dest", help="Set an alternative destination directory (full path)", action="store")
 args = parser.parse_args()
 
 if args.move:
     print "Will MOVE files"
 
+if args.dest:
+  destDir = args.dest
+
+print "Will copy files to %s" % (args.dest) 
 print "Will process: ", args.pathString
 sourceDir = args.pathString
 
@@ -77,6 +83,7 @@ for root, dirs, photos in os.walk(sourceDir):
           # We need to get the full path to the sub directory
           fullpath = os.path.join(root, photo)
           original = fullpath
+          extension = os.path.splitext(original)[1]
           suffix = 'a'
 
           try:
@@ -98,12 +105,12 @@ for root, dirs, photos in os.walk(sourceDir):
             if not os.path.exists(thisDestDir):
               os.makedirs(thisDestDir)
 
-            duplicate = thisDestDir + '/%s.jpg' % (newname) # This needs fixing !!!!!!!!
+            duplicate = thisDestDir + '/%s%s' % (newname, extension)
 
             # Fix identical timestamps with a suffix
             while os.path.exists(duplicate):
               newname = pDate.strftime(fmt) + suffix
-              duplicate = destDir + '/%04d/%02d/%s.jpg' % (yr, mo, newname)
+              duplicate = destDir + '/%04d/%02d/%s%s' % (yr, mo, newname, extension)
               suffix = chr(ord(suffix) + 1)
               
             # Copy or Move the files
